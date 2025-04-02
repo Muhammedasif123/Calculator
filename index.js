34f637996ab8792374bb14e7278cb435cdddb196
Muhammedasif123 <muhammedasif13577@gmail.com>
2025-04-02 17:39:31 +0530

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-let display=document.getElementById('display');
+let display = document.getElementById('display');
 
-let button= Array.from(document.getElementsByClassName('button'));
+let buttons = Array.from(document.getElementsByClassName('button'));
 
 buttons.map( button => {
-    button.addEventlistener('click',(e) => {
-        console.log('clicked');
+    console.log("tappped")
+    button.addEventListener('click', (e) => {
+       switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+            case '⇚':
+                if(display.innerText){
+                    display.innerText = display.innerText.slice(0,-1);
+                }
+                break;
+            case '=':
+            try{
+                display.innerText = eval(display.innerText); 
+            } catch {
+                display.innerText = 'Error!';
+            }
+             break;
+         default:
+             display.innerText += e.target.innerText;
+
+       }
 
     })
 })
